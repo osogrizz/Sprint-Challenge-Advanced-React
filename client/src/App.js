909c3419f767403e0e28c8f6bd33d937a9cc45ea
osogrizz <lhtor@gmail.com>
2019-11-10 18:36:16 -0800

    Fetches player data from API.

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,33 @@ export class App extends Component {
   constructor() {
     super()
 
-    this.state = {}
+    this.state = {
+      players: []
+    }
+  }
+
+  componentDidMount() {
+    const getPlayers = () =>{
+      fetch('http://localhost:5000/api/players')
+      .then(res => res.json())
+      .then(players => {
+        console.log(players)
+        this.setState({
+          players: players
+        })
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    }
+    getPlayers()
   }
 
 
   render() {
     return (
       <div>
-        
+
       </div>
     )
   }
